301d2614433eda54a2402a146ee8802af003c451
Goodron1981 <Ponamarev7@gmail.com>
2017-04-13 17:41:03 +0300

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,27 @@
+/**
+ * Created by chernyahivskiy.v on 11.04.2017.
+ */
+public class Human {
+    String name;
+    int age;
+    Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
